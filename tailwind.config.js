681dc0159da3eaa3d24e944c7c3fd98cfef97e06
Vincent De Borger <hello@vincentdeborger.be>
2021-08-24 18:07:16 +0200

    Add current theme pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,54 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h2: {
+              fontWeight: '800'
+            },
+            h3: {
+              fontWeight: '800'
+            },
+          },
+        },
+      })
+    },
   },
   variants: {
-    extend: {},
+    marginBottom: ({ after }) => after(["last-of-type"])
   },
-  plugins: [],
+  corePlugins: {
+    container: false
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '600px',
+          },
+          '@screen md': {
+            maxWidth: '700px',
+          },
+          '@screen lg': {
+            maxWidth: '800px',
+          },
+          '@screen xl': {
+            maxWidth: '900px',
+          },
+        }
+      })
+    },function ({ addVariant, e }) {
+      addVariant("last-of-type", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(`last-of-type${separator}${className}`)}:last-of-type`;
+        });
+      });
+    },
+    require('@tailwindcss/typography'),
+  ]
 }
